055062571e66e099bfa46ee41bd4179dfeb323cf
iamgraypix <charlesartillero@gmail.com>
2024-05-25 22:11:57 +0800

    changes

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php
 
 require 'functions.php';
-
+require 'Database.php';
 // require 'router.php';
 
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-$pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("select * from posts");
+$db = new Database();
 
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
     echo "<li>{$post['title']}</li>";
