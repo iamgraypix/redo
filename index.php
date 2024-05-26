4b0da284779fb624c9ed852de247061aedfcb7c5
iamgraypix <charlesartillero@gmail.com>
2024-05-26 14:18:06 +0800

    update

--- index.php
+++ index.php
@@ -2,17 +2,6 @@
 
 require 'functions.php';
 require 'Database.php';
-// require 'router.php';
+require 'router.php';
 
-$config = require 'config.php';
 
-$db = new Database($config['database']);
-
-$id = $_GET['id'];
-
-$posts = $db->query("select * from posts where id = :id", 
-["id" => $id])->fetchAll();
-
-foreach ($posts as $post) {
-    echo "<li>{$post['title']}</li>";
-}
