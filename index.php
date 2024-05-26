69f1febfdca542e67d08554e36ea2e371d7962f4
iamgraypix <charlesartillero@gmail.com>
2024-05-26 12:01:40 +0800

    separate params in sql query to avoid sql injection

--- index.php
+++ index.php
@@ -8,7 +8,10 @@ $config = require 'config.php';
 
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+
+$posts = $db->query("select * from posts where id = :id", 
+["id" => $id])->fetchAll();
 
 foreach ($posts as $post) {
     echo "<li>{$post['title']}</li>";
