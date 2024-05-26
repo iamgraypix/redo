4bc940b0d8a968b7dd91700682c66b839efc28a0
iamgraypix <charlesartillero@gmail.com>
2024-05-26 14:44:28 +0800

    add authorization

--- controllers/note.php
+++ controllers/note.php
@@ -4,9 +4,17 @@ $heading = 'Note';
 
 $config = require 'config.php';
 $db = new Database($config['database']);
-
+$currentUser = 3;
 
 $note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $_GET['id']])->fetch();
+
+if (!$note) {
+    abort();
+}
+
+if ($note['user_id'] !== $currentUser) {
+    abort(Response::FORBIDDEN);
+}
 
 
 require 'views/note.view.php';
