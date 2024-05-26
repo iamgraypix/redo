a93ffb1c9e68f9d98a167dc36452569aed038e43
iamgraypix <charlesartillero@gmail.com>
2024-05-26 14:18:32 +0800

    new files

---
+++ controllers/note.php
@@ -0,0 +1,12 @@
+<?php
+
+$heading = 'Note';
+
+$config = require 'config.php';
+$db = new Database($config['database']);
+
+
+$note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $_GET['id']])->fetch();
+
+
+require 'views/note.view.php';
