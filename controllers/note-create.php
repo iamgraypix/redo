5cd2bce75f97e7fa4472410e77a4da7727ce3591
iamgraypix <charlesartillero@gmail.com>
2024-05-28 19:33:19 +0800

    update

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,25 +2,16 @@
 
 $heading = 'Create Note';
 $config = require 'config.php';
+require 'Validator.php';
 $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $body = $_POST['body'];
     $errors = [];
 
-    if (empty(trim($body))){
+    if (! Validator::string($body, 3, 1000)){
         http_response_code(400);
-        $errors['body'] = 'A body is required!'; 
-    }
-
-    if (strlen($body) > 1000){
-        http_response_code(400);
-        $errors['body'] = 'A body cannot be more than 1,000 characters!';
-    }
-
-    if (strlen(trim($body)) <= 3){
-        http_response_code(400);
-        $errors['body'] = 'A body cannot be less than 3 characters!';
+        $errors['body'] = 'A body cannot be more than 1,000 characters is required!';
     }
 
     if (empty($errors)){
