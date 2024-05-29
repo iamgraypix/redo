a08a3c4207261ff23032e2c04d062367eb9aec6d
iamgraypix <charlesartillero@gmail.com>
2024-05-29 12:25:15 +0800

    update

--- Core/router.php
+++ Core/router.php
@@ -2,19 +2,19 @@
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes = require 'routes.php';
+$routes = require base_path('routes.php');
 
 function abort($code = 404)
 {
     http_response_code($code);
-    require "views/{$code}.view.php";
+    require base_path("views/{$code}.view.php");
     die();
 }
 
 function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
