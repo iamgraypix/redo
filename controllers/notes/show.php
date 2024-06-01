73256813f3011ee0315c1dff4b119ac6c820b5e2
iamgraypix <charlesartillero@gmail.com>
2024-06-01 11:30:39 +0800

    update

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -6,12 +6,23 @@ $config = require base_path('config.php');
 $db = new Database($config['database']);
 $currentUser = 3;
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $_GET['id']])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $_GET['id']])->findOrFail();
+    authorize($note['user_id'] === $currentUser);
 
-authorize($note['user_id'] === $currentUser);
+    $db->query("DELETE FROM notes WHERE id = :id", ['id' => $_POST['id']]);
+
+    header('location: /notes');
+
+} else {
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $_GET['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUser);
 
 
-view('notes/show.view.php', [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    view('notes/show.view.php', [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
