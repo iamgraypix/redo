714a1b8d62d21402fb999f288708e39177927033
iamgraypix <charlesartillero@gmail.com>
2024-06-04 22:25:19 +0800

    update

--- Core/functions.php
+++ Core/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Response;
+use Core\Session;
 
 function dd($value)
 {
@@ -51,10 +52,7 @@ function login($user)
 
 function logout()
 {
-    session_destroy();
-
-    $params = session_get_cookie_params();
-    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+    Session::destroy();
 }
 
 function redirect($path)
