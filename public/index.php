912c6dc48a68083ad44c6825babda09e722c2509
iamgraypix <charlesartillero@gmail.com>
2024-06-06 15:46:44 +0800

    update

--- public/index.php
+++ public/index.php
@@ -3,14 +3,14 @@
 use Core\Session;
 use Core\ValidationException;
 
+
 session_start();
 
 const BASE_PATH = __DIR__ . '../../';
+require BASE_PATH . '/vendor/autoload.php';
+
 require BASE_PATH . 'Core/functions.php';
 
-spl_autoload_register(function ($class) {
-    require base_path("{$class}.php");
-});
 
 require base_path('boostrap.php');
 
